43fc5818f6ad5846e8ef491860a6c3a73f079188
bcamacho3 <bcama@SwordFish>
2019-05-10 23:14:00 -0700

    b

---
+++ src/Decorator/TesterRoom.java
@@ -0,0 +1,16 @@
+package Decorator;
+
+import Room.AquaRoom;
+
+public class TesterRoom {
+
+	public static void main(String[] args) {
+		// test aquaRoom
+		AquaRoom aRoom1 = new AquaRoom();
+		
+		System.out.println(aRoom1.toString());
+		
+		
+	}
+
+}
